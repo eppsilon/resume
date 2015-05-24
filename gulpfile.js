84dd53eb882d3d3fb6b05ad4b1d5d109e466615f
Brett Epps <brett@eppsilon.com>
2015-05-24 14:42:13 -0500

    Sorta kinda got livereload to work.

--- gulpfile.js
+++ gulpfile.js
@@ -1,35 +1,59 @@
 'use strict';
 
 var gulp = require('gulp');
-var server = require('gulp-server-livereload');
-var minifyCss = require('gulp-minify-css');
 var gutil = require('gulp-util');
-
+var watch = require('gulp-watch');
+var minifyCss = require('gulp-minify-css');
 var less = require('gulp-less');
-var LessPluginAutoPrefix = require('less-plugin-autoprefix'),
-    autoprefix = new LessPluginAutoPrefix({ browsers: ['last 2 versions'] });
+var LessPluginAutoPrefix = require('less-plugin-autoprefix');
+var autoprefix = new LessPluginAutoPrefix({ browsers: ['last 2 versions'] });
+var livereload = require('gulp-livereload');
+var server = require('gulp-webserver');
 
-gulp.task('default', ['css', 'html']);
-gulp.task('srv-default', ['default', 'serve']);
+gulp.task('default', ['css', 'js', 'html']);
+gulp.task('dev', ['watch']);
+
+gulp.task('watch', function() {
+  gulp.src('dist')
+    .pipe(server({
+      livereload: true,
+      open: true
+    }));
+
+  gulp.src('src/css/*.less')
+    .pipe(watch('src/css/*.less', function() {
+      gulp.start('css');
+    }, { verbose: true }));
+
+  gulp.src('src/js/*.js')
+    .pipe(watch('src/js/*.js', function() {
+      gulp.start('js');
+    }, { verbose: true }));
+
+  gulp.src('src/*.html')
+    .pipe(watch('src/*.html', function() {
+      gulp.start('html');
+    }, { verbose: true }));
+});
 
 gulp.task('css', function() {
-  return gulp.src('./src/css/main.less')
+  return gulp.src('src/css/*.less')
     .pipe(less({ plugins: [autoprefix] }))
+    .on('error', gutil.log)
     .pipe(minifyCss())
-      .on('error', gutil.log)
-    .pipe(gulp.dest('./dist/css'));
+    .pipe(gulp.dest('dist/css'))
+    .on('error', gutil.log)
+    .pipe(livereload());
+});
+
+gulp.task('js', function() {
+  return gulp.src('src/js/*.js')
+    .pipe(gulp.dest('dist/js'))
+    .pipe(livereload());
 });
 
 gulp.task('html', function() {
-  return gulp.src('./src/index.html')
-    .pipe(gulp.dest('./dist'));
-});
- 
-gulp.task('serve', function() {
-  gulp.src('./dist')
-    .pipe(server({
-      livereload: true,
-      directoryListing: true,
-      open: true
-    }));
+  return gulp.src('src/*.html')
+    .pipe(gulp.dest('dist'))
+    .pipe(livereload());
 });
