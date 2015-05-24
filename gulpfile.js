b6bf0d85421ab932f2a039d02c8968a9c3a96107
Brett Epps <brett@eppsilon.com>
2015-05-24 11:37:22 -0500

    Added devel web server task to gulpfile.js. Moved source code to own directory.

--- gulpfile.js
+++ gulpfile.js
@@ -1,21 +1,35 @@
 'use strict';
 
 var gulp = require('gulp');
-var less = require('gulp-less');
+var server = require('gulp-server-livereload');
 var minifyCss = require('gulp-minify-css');
 var gutil = require('gulp-util');
 
+var less = require('gulp-less');
+var LessPluginAutoPrefix = require('less-plugin-autoprefix'),
+    autoprefix = new LessPluginAutoPrefix({ browsers: ['last 2 versions'] });
+
 gulp.task('default', ['css', 'html']);
+gulp.task('srv-default', ['default', 'serve']);
 
 gulp.task('css', function() {
-  return gulp.src('css/main.less')
-    .pipe(less())
+  return gulp.src('./src/css/main.less')
+    .pipe(less({ plugins: [autoprefix] }))
     .pipe(minifyCss())
       .on('error', gutil.log)
     .pipe(gulp.dest('./dist/css'));
 });
 
 gulp.task('html', function() {
-  return gulp.src('index.html')
+  return gulp.src('./src/index.html')
     .pipe(gulp.dest('./dist'));
+});
+ 
+gulp.task('serve', function() {
+  gulp.src('./dist')
+    .pipe(server({
+      livereload: true,
+      directoryListing: true,
+      open: true
+    }));
 });
